57c6391379088dcac8b5382702296e67ee4a4c96
Javarris Barnett <javarrisb@gmail.con>
2021-12-16 22:56:53 +0100

    Inheritance to player and enemy

--- jest/lib/Enemy.js
+++ jest/lib/Enemy.js
@@ -1,20 +1,20 @@
 const Potion = require('./Potion');
 const Character = require('./Character');
 
-class Enemy {
+class Enemy extends Character {
     constructor(name, weapon) {
-    this.name = name;
+        // call parent constructor here:
+        super(name);
+
     this.weapon = weapon;
     this.potion = new Potion();
-
-    this.health = Math.floor(Math.random() * 10 + 85);
-    this.strength = Math.floor(Math.random() * 5 + 5);
-    this.agility = Math.floor(Math.random() * 5 + 5);
 }
 
 getDescription() {
     return `A ${this.name} holding a ${this.weapon} has appeared!`;
 };
 }
+
+
 
 module.exports = Enemy;
